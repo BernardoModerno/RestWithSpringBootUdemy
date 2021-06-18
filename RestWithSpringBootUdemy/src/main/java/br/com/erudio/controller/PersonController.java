3990149bccbfe9fdd0606c0bc1173b4f5ab21e5e
BernardoModerno <bernardo.moderno@gmail.com>
2021-06-18 19:50:08 -0300

    Voltando ao projeto sem V2

--- RestWithSpringBootUdemy/src/main/java/br/com/erudio/controller/PersonController.java
+++ RestWithSpringBootUdemy/src/main/java/br/com/erudio/controller/PersonController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.data.vo.PersonVO;
-import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonServices;
 
 @RestController
@@ -37,11 +36,6 @@ public class PersonController {
 	@PostMapping
 	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
-	}
-	
-	@PostMapping("/v2")
-	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
-		return service.createV2(person);
 	}
 	
 	@PutMapping
