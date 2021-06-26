df90424401b05c0b0c0b5e68fe4d304af635ce1a
BernardoModerno <bernardo.moderno@gmail.com>
2021-06-26 15:59:10 -0300

    O Verbo PATCH

--- RestWithSpringBootUdemy/src/main/java/br/com/erudio/exception/InvalidJwtAuthenticationException.java
+++ RestWithSpringBootUdemy/src/main/java/br/com/erudio/exception/InvalidJwtAuthenticationException.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class InvalidJwtAuthenticationException extends AuthenticationException{
 
 	private static final long serialVersionUID = 1L;
-
+	
 	public InvalidJwtAuthenticationException(String exception) {
 		super(exception);
 	}
-
+	
 }
